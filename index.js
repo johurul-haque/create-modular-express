23b58cb58de1bb0770068a3be0a2975ee038cd0f
Johurul Haque <johurulhaquejony@gmail.com>
2023-12-30 12:06:34 +0600

    not excluding the .env.example file

--- index.js
+++ index.js
@@ -45,8 +45,13 @@ async function runCli(destination) {
     await fs.copy(`${__dirname}/template`, destination, {
       filter: (src) => {
         const exclusions = ['node_modules', 'dist', '.vercel', '.env'];
+        const fileName = path.basename(src);
 
-        return !exclusions.some((exclusion) => src.includes(exclusion));
+        // Exclude .env file but include .env.example
+        return (
+          fileName !== '.env' &&
+          !exclusions.some((exclusion) => src.includes(exclusion))
+        );
       },
     });
 
