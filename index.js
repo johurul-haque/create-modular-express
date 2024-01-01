7d23f5f0fb06f6bbe9fe22aa87ced5a89ec43ba1
Johurul Haque <johurulhaquejony@gmail.com>
2024-01-01 10:30:24 +0600

    removed console statement in the catch block

--- index.js
+++ index.js
@@ -78,7 +78,6 @@ async function runCli(destination) {
     `);
   } catch (error) {
     spinner.error({ text: 'ERROR: Something went wrong!' });
-    console.log(error);
     process.exit(1);
   }
 }
