f15aa48bbd8f57043965724438264102cc5f2539
Johurul Haque <johurulhaquejony@gmail.com>
2024-05-23 01:01:32 +0600

    bug fixes: not having the next param in globalCatch causes it not work. Weird!

--- template/src/middlewares/global-catch.ts
+++ template/src/middlewares/global-catch.ts
@@ -1,6 +1,6 @@
 import * as handle from '@helpers/handle-errors';
 import { AppError } from '@utils';
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { ErrorResponse } from 'interface/errors';
 import mongoose from 'mongoose';
 import { ZodError } from 'zod';
@@ -23,7 +23,8 @@ export function globalCatch(
     | AppError
     | Error,
   req: Request,
-  res: Response
+  res: Response,
+  next: NextFunction
 ) {
   if (error instanceof ZodError) {
     const zError = handle.zodError(error);
